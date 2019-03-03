73e9fc843c09eb42f15ba8f77f26bb43576b9cb2
percy1897 <48006716+percy1897@users.noreply.github.com>
2019-03-04 00:30:13 +0530

    added spinner

--- registerpage.java
+++ registerpage.java
@@ -9,5 +9,12 @@ public class registerpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registerpage);
+        
+        
+        Spinner mySpinner=(Spinner)findViewById(R.id.spinner2);
+        ArrayAdapter<String> myAdapter= new ArrayAdapter<String>(registerpage.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.BloodGroup2));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
 }
